1df3aac9430adcd54dcfb3d72718a61412e27796
freddy248 <fredrick.skio.nyirenda@gmail.com>
2025-10-17 02:17:25 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // === Configure these two values ===
 window.CONFIG = {
-  DATA_API: "https://script.google.com/macros/s/AKfycbz4hE8vHlJ-acnhaELwBdG3D97BP44FP5uG5bVMS2Y37B21-Xgnmuc4yT0Zn93QSQGXKw/exec",
-  API_KEY:  "fredricknyirendaelse@248",   
+  DATA_API: "hhttps://script.google.com/macros/s/AKfycbyCSB-T6IMrk0JrXHAxjC7bwg-39X8NWrRgkAq6lWaK7j8MHC_2_jT0CxO-U_c29UcLAg/exec",
+  API_KEY:  "a1b2c3d4e5f6-7g8h9i0j-1k2l3m4n5o6p7q8r9s0t",   
   USE_BACKEND: true, 
 };
